73efb5f0037d3a30fa64a838e81a4c776ff5d47c
FaiyazAliMohammed <166060571+FaiyazAliMohammed@users.noreply.github.com>
2024-05-01 02:35:56 +0530

    Initial commit

---
+++ src/test/java/Project/Test01.java
@@ -0,0 +1,24 @@
+package Project;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test01 {
+
+    @Test
+    @Owner("Hasan Ali")
+    @Description ("This is simple test case")
+
+    public void titlePage(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+
+    }
+}
