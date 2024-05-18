260d9c3e00571a34220f10c12becc4d77dd15a5d
FaiyazAliMohammed <166060571+FaiyazAliMohammed@users.noreply.github.com>
2024-05-19 04:43:57 +0530

    my third commit

--- src/test/java/Project/PraticeTestCase/selenium/Selenium10.java
+++ src/test/java/Project/PraticeTestCase/selenium/Selenium10.java
@@ -10,7 +10,7 @@ public class Selenium10 {
     @Test
     @Description("Verify the current url and title of Visascentral.com")
 
-            public void NewTC(){
+            public void NewTC() throws InterruptedException {
         // open visascentral.com
         // print the title and get urrent url
         // verifi the current url is visascentral.con
@@ -19,6 +19,8 @@ public class Selenium10 {
         driver.get("https://visascentral.com/");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
+
+        Thread.sleep(5000);
 
         Assert.assertEquals(driver.getTitle(),"Visa Central – Visas");
         Assert.assertEquals(driver.getCurrentUrl(),"https://visascentral.com/");
